d1f5f187a96b633063d7703048de2f7cc310765a
Gora2ks <gora2ks@gmail.com>
2019-11-04 23:59:15 +0100

    Update homepage.java

--- Test_web_app_java/homepage.java
+++ Test_web_app_java/homepage.java
@@ -1,4 +1,5 @@
 package ryanairrepository;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -7,7 +8,7 @@ import org.openqa.selenium.support.PageFactory;
 public class homepage {
     WebDriver driver;
 
-    // Home Page class implemented in Page object factory method
+    // Home Page class implemented in Page Object Factory method
     public homepage(WebDriver driver) {
         this.driver = driver;
         PageFactory.initElements(driver, this);
@@ -23,38 +24,62 @@ public class homepage {
     //Destination
     @FindBy(css = "input[placeholder='Destination airport']")
     WebElement destination;
-
+    // Day of the Deoarture
     @FindBy(css = "li[data-id*='15-11']")
     WebElement date_of_departure;
+    //Book one or more one passengers
+    @FindBy(css = "div[class='dropdown-handle']")
+    WebElement passengers_input;
+    //Chose how many of passengers will go
+    @FindBy(css = "div[value*='paxInput.adults']")
+    WebElement input_adults;
 
-    public WebElement ticket_to_one_way()
-    {
+    @FindBy(css = "div[value*='paxInput.teens']")
+    WebElement input_teens;
+
+    @FindBy(css = "div[value*='paxInput.children']")
+    WebElement input_children;
+
+    @FindBy(css = "div[value*='paxInput.infants']")
+    WebElement input_infants;
+
+    public WebElement Ticket_to_one_way() {
         return ticket_to_one_way;
     }
 
-    public WebElement destination()
-    {
+    public WebElement Destination() {
         return destination;
     }
 
-    public WebElement departure()
-    {
+    public WebElement Departure() {
         return departure;
     }
 
-    public WebElement date_of_departure()
-    {
+    public WebElement Date_of_departure() {
         return date_of_departure;
     }
 
-
-//    public WebElement Departure() {
-//        return driver.findElement(destination);
-//    }
-//
-//    public WebElement Destination() {
-//        return driver.findElement(departure);
+    public WebElement Passengers_input() {
+        return passengers_input;
     }
+
+    public WebElement Input_adults() {
+        return input_adults;
+    }
+
+    public WebElement Input_teens() {
+        return input_teens;
+    }
+
+    public WebElement Input_children() {
+        return input_children;
+    }
+
+    public WebElement Input_infants() {
+        return input_infants;
+    }
+
+}
 
 
 
