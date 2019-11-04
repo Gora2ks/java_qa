d0f4b779e23a965d548bad253340c1d65702059a
Gora2ks <gora2ks@gmail.com>
2019-11-04 23:58:28 +0100

    Update test_web_app.java

--- Test_web_app_java/test_web_app.java
+++ Test_web_app_java/test_web_app.java
@@ -1,19 +1,19 @@
-import net.bytebuddy.asm.Advice;
 import org.junit.BeforeClass;
-import org.openqa.selenium.*;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 import ryanairrepository.homepage;
 import ryanairrepository.loginPage;
-
 import java.util.concurrent.TimeUnit;
+// Test_web_app class implemented in normal Page object pattern style
 
 public class test_web_app {
     @BeforeClass
     public static void setup() {
         System.setProperty("webdriver.chrome.driver", "/home/ute/Documents/Work/Develop/Python/webdriver_my/chromedriver");
         WebDriver driver = new ChromeDriver();
-
         //       driver.manage().deleteAllCookies();
         driver.manage().timeouts().implicitlyWait(30,
                 TimeUnit.SECONDS);
@@ -35,22 +35,59 @@ public class test_web_app {
         lp.SubmitId().click();
         {
             homepage hp = new homepage(driver);
-        Thread.sleep(1000);
-        hp.ticket_to_one_way().click();
-        hp.departure().sendKeys("wroclaw");
-        hp.departure().sendKeys(Keys.ENTER);
-        hp.destination().sendKeys("paris B");
-        hp.destination().sendKeys(Keys.ENTER);
-        Thread.sleep(1000);
-        hp.date_of_departure().click();
+            Thread.sleep(2000);
+            hp.Ticket_to_one_way().click();
+//        hp.departure().click();
+            hp.Departure().sendKeys("wroclaw");
+            hp.Departure().sendKeys(Keys.ENTER);
+            hp.Destination().sendKeys("paris B");
+            hp.Destination().sendKeys(Keys.ENTER);
+            Thread.sleep(1000);
+            hp.Date_of_departure().click();
+            hp.Passengers_input().click();
+            Thread.sleep(2000);
+            for (int i = 1; i < 2; i++) {
+                hp.Input_adults().click();
+            }
+            for (int i = 0; i < 2; i++) {
+                hp.Input_teens().click();
+            }
+            for (int i = 0; i < 1; i++) {
+                hp.Input_children().click();
+            }
+            for (int i = 0; i < 1; i++) {
+                hp.Input_infants().click();
+            }
 
 
         }
     }
 
+//        WebElement overlay = driver.findElement(By.xpath(".//input[@name='password']"));
+//        js.executeScript("arguments[0].click();", overlay);;
+//        overlay.sendKeys("123");
+//        System.out.printf(" go");
+//        lp.SubmitId().click();
 
 
- 
+//        Assert.assertTrue(driver.findElement(By.cssSelector("#checkBoxOption1")).isSelected());
+//        Assert.assertFalse(driver.findElement(By.cssSelector("#checkBoxOption1")).isSelected())
+
+
+//    @Test(priority = 3)
+//    void homepage() {
+//        WebDriver driver = new ChromeDriver();
+//        homepage hp = new homepage(driver);
+//
+//
+//
+//        System.out.printf("gogodz");
+//    }
+
+    @Test(priority = 1)
+    void pay() {
+        int apple = 5;
+        System.out.println(apple + 3.5);
 
 
     }
