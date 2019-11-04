388607656bf946e7d02ba1200099e67910c06747
Gora2ks <gora2ks@gmail.com>
2019-11-04 22:32:03 +0100

    Update test_web_app.java

--- Test_web_app_java/test_web_app.java
+++ Test_web_app_java/test_web_app.java
@@ -1,60 +1,59 @@
-package ryanairrepository;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.PageFactory;
+import net.bytebuddy.asm.Advice;
+import org.junit.BeforeClass;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+import ryanairrepository.homepage;
+import ryanairrepository.loginPage;
 
-public class homepage {
-    WebDriver driver;
+import java.util.concurrent.TimeUnit;
 
-    // Home Page class implemented in Page object factory method
-    public homepage(WebDriver driver) {
-        this.driver = driver;
-        PageFactory.initElements(driver, this);
+public class test_web_app {
+    @BeforeClass
+    public static void setup() {
+        System.setProperty("webdriver.chrome.driver", "/home/ute/Documents/Work/Develop/Python/webdriver_my/chromedriver");
+        WebDriver driver = new ChromeDriver();
+
+        //       driver.manage().deleteAllCookies();
+        driver.manage().timeouts().implicitlyWait(30,
+                TimeUnit.SECONDS);
     }
 
-    //Departure
-    @FindBy(css = "span[id='lbl-flight-search-type-one-way']")
-    WebElement ticket_to_one_way;
-
-    @FindBy(css = "input[placeholder='Departure airport']")
-    WebElement departure;
-
-    //Destination
-    @FindBy(css = "input[placeholder='Destination airport']")
-    WebElement destination;
-
-    @FindBy(css = "li[data-id*='15-11']")
-    WebElement date_of_departure;
-
-    public WebElement ticket_to_one_way()
-    {
-        return ticket_to_one_way;
-    }
-
-    public WebElement destination()
-    {
-        return destination;
-    }
-
-    public WebElement departure()
-    {
-        return departure;
-    }
-
-    public WebElement date_of_departure()
-    {
-        return date_of_departure;
-    }
+    @Test(priority = 1)
+    public void Login() throws InterruptedException {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://www.ryanair.com/ie/en/");
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        loginPage lp = new loginPage(driver);
+        lp.Login().click();
+        Thread.sleep(1000);
+        lp.EmailId().click();
+        lp.EmailId().sendKeys("gora2ks@gmail.com");
+        lp.Eye().click();
+        lp.PasswordId().sendKeys("Gogodz78600");
+        lp.SubmitId().click();
+        {
+            homepage hp = new homepage(driver);
+        Thread.sleep(1000);
+        hp.ticket_to_one_way().click();
+        hp.departure().sendKeys("wroclaw");
+        hp.departure().sendKeys(Keys.ENTER);
+        hp.destination().sendKeys("paris B");
+        hp.destination().sendKeys(Keys.ENTER);
+        Thread.sleep(1000);
+        hp.date_of_departure().click();
 
 
-//    public WebElement Departure() {
-//        return driver.findElement(destination);
-//    }
-//
-//    public WebElement Destination() {
-//        return driver.findElement(departure);
+        }
     }
 
 
+
+ 
+
+
+    }
+
+}
 
